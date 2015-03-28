da862331063ca57387ea812726c173661269712f
Torrib <torrib90@gmail.com>
2015-03-28 13:00:28 +0100

    Working on powerups

---
+++ tdt4240/tdt4240/PowerUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tdt4240
+{
+
+    enum Target
+    {
+        None,
+        Self,
+        Enemy,
+        All
+    }
+
+    abstract class PowerUp
+    {
+        protected Target target;
+        protected event EventHandler<PowerUpEvent> effect;
+    }
+}
---
+++ tdt4240/tdt4240/PowerUpEvent.cs
@@ -0,0 +1,31 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// PlayerIndexEventArgs.cs
+//
+// XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using Microsoft.Xna.Framework;
+#endregion
+
+namespace tdt4240
+{
+    class PowerUpEvent : EventArgs
+    {
+        public PowerUpEvent(Player player)
+        {
+            this.player = player;
+        }
+
+        public Player Player
+        {
+            get { return player; }
+        }
+
+        Player player;
+    }
+}
