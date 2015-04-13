692f3b9724feddaf96d1fc0c0bbba23e93bbea26
Aina Elisabeth Thunestveit <aina.elisabeth.thunestveit@gmail.com>
2015-04-13 14:46:18 +0200

    Added Equation object

---
+++ tdt4240/tdt4240/Minigames/Mathgame/Equation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using tdt4240.Assets;
+
+namespace tdt4240.Minigames.Mathgame
+{
+    class Equation
+    {
+        private Equation _equation;
+        private Boolean _correctAnswer;
+
+        //Constructor
+        public Equation ()
+        {
+            //TODO get equation from problem
+            //TODO get correctAnswer from problem
+        }
+
+        public Equation equation
+        {
+            get { return _equation; }
+            set { _equation = value; }
+        }
+
+        public Boolean CorrectAnswer
+        {
+            get { return _correctAnswer; }
+            set { _correctAnswer = value; }
+        }
+    }
+}
