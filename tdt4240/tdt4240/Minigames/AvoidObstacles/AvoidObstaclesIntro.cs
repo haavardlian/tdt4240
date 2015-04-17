967dcfaba3d40dfb3c5a95cef4d0e43f56ca8c00
Espen Hellerud <espenhellerud5@gmail.com>
2015-04-17 14:07:17 +0200

    added intro to avoid obstacles or Birdhunt

---
+++ tdt4240/tdt4240/Minigames/AvoidObstacles/AvoidObstaclesIntro.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using tdt4240.Boards;
+
+namespace tdt4240.Minigames.AvoidObstacles
+{
+    class AvoidObstaclesIntro : MinigameIntro
+    {
+        public new static SupportedPlayers SupportedPlayers = SupportedPlayers.All;
+
+        private ContentManager _content;
+
+
+        public AvoidObstaclesIntro(Board board) : base(board)
+        {
+            MiniGame = new AvoidObstacles(board);
+
+            ThumbstickDescription = "Use thumbstick do speed the helicopter in different directions";
+            GameDescription = "Avoid birds and the other helicopters";
+            Goal = "TBA";
+
+            /*ControllerButtons.Add(Buttons.A, "Exit game");
+            ControllerButtons.Add(Buttons.B, "Test 1");
+            ControllerButtons.Add(Buttons.Y, "Test 2");
+
+            KeyboardButtons.Add(Keys.D1, "Exit game");
+            KeyboardButtons.Add(Keys.Back, "Test 1");
+            KeyboardButtons.Add(Keys.Y, "Test 2");*/
+        }
+
+        public override void Activate(bool instancePreserved)
+        {
+            base.Activate(instancePreserved);
+
+            if (!instancePreserved)
+            {
+                if (_content == null)
+                    _content = new ContentManager(ScreenManager.Game.Services, "Content");
+
+                Cover = _content.Load<Texture2D>("minigameCovers/demo");
+            }
+        }
+    }
+}
---
+++ tdt4240/tdt4240/Minigames/BirdHunt/BirdHuntIntro.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using tdt4240.Boards;
+
+namespace tdt4240.Minigames.BirdHunt
+{
+    class BirdHuntIntro : MinigameIntro
+    {
+        public new static SupportedPlayers SupportedPlayers = SupportedPlayers.All;
+
+        private ContentManager _content;
+
+
+        public BirdHuntIntro(Board board) : base(board)
+        {
+            MiniGame = new BirdHunt(board);
+
+            ThumbstickDescription = "Move the crosshair around";
+            GameDescription = "aim with the crosshair and shoot the birds";
+            Goal = "TBA";
+
+            ControllerButtons.Add(Buttons.A, "Shoot");
+            KeyboardButtons.Add(Keys.D1, "Shoot");
+
+
+            /*ControllerButtons.Add(Buttons.A, "Exit game");
+            ControllerButtons.Add(Buttons.B, "Test 1");
+            ControllerButtons.Add(Buttons.Y, "Test 2");
+
+            KeyboardButtons.Add(Keys.D1, "Exit game");
+            KeyboardButtons.Add(Keys.Back, "Test 1");
+            KeyboardButtons.Add(Keys.Y, "Test 2");*/
+        }
+
+        public override void Activate(bool instancePreserved)
+        {
+            base.Activate(instancePreserved);
+
+            if (!instancePreserved)
+            {
+                if (_content == null)
+                    _content = new ContentManager(ScreenManager.Game.Services, "Content");
+
+                Cover = _content.Load<Texture2D>("minigameCovers/demo");
+            }
+        }
+    }
+}
